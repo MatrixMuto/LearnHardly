231c58d4f02ea0928f4f560e678cbadad8830768
MatrixMuto <wang.qiang.mu@gmail.com>
2016-03-05 16:25:41 +0800

    change to 1 ray array,but no use

--- Debug/ACM_ZOJ/zoj_3925/main.cpp
+++ Debug/ACM_ZOJ/zoj_3925/main.cpp
@@ -8,9 +8,9 @@ using namespace std;
 int main(int argc, char** argv) {
 		int* R = new int[1000];
         int* B = new int[1000];
-        int (*ans)[1001]= new int[1001][1001];
+//        int (*ans)[1001]= new int[1001][1001];
 //        int* ans = new int[1001*1001]; 
-//		int** ans =(int**) malloc(1001*1001*sizeof(int));
+		int* ans =(int*) malloc(1001*1001*sizeof(int));
         int T = 0;
         //string color="";
 		char color[1001];
@@ -47,8 +47,8 @@ int main(int argc, char** argv) {
                                 best = B[j]<(R[j]+K)?B[j]:(R[j]+K);
                             }
                     }
-                    ans[i][n] = best;
-//                    ans[i*N+n] = best;
+//                    ans[i][n] = best;
+                    ans[i*N+n] = best;
                     //cout << "ans[" << i << "][" << n << "]=" << best<< endl; 
                 }
             }
@@ -57,19 +57,29 @@ int main(int argc, char** argv) {
                     best = 2147483647;
                     //j==i
                     j = i;
-                    temp = ans[j][1] + ans[j + 1][i + n - 1 - j];
+//                    temp = ans[j][1] + ans[j + 1][i + n - 1 - j];
+                    temp = ans[i*N+1] + ans[(j+1)*N+i+n-1-j];
                     if(temp < best)
                         best = temp;
                     //j==i+n-1
                     j = i+n-1;
-                    temp = ans[j][1] + ans[i][j - i];
+//                    temp = ans[j][1] + ans[i][j - i];
+                    temp = ans[i*N+1] + ans[i*N +j -i];
                     if(temp < best)
                         best = temp;
+					int* ans_i = ans+ i*N;
+					int* ans_j = ans_i;
                     for (j = i+1; j <= i+n-1-1; j++) {
 //                            left = -1;
 //                            right = -1;
-                            left = ans[i][j - i];
-                            right = ans[j + 1][i + n - 1 - j];
+//                            left = ans[i][j - i];
+//							temp = ans[j*N+1];
+							temp = *(ans_j+1);
+							ans_j += N;
+							left = *(ans_i + j-i);
+//                            right = ans[j + 1][i + n - 1 - j];
+//							right = ans[(j+1)*N+i+n-1-j];
+							right = *(ans_j+i+n-1-j); 
 //                            if (j - 1 >= i) {
 //                                left = ans[i][j - i];
 ////                                left = ans[i*N +j -i];
@@ -81,18 +91,18 @@ int main(int argc, char** argv) {
 ////                                right = ans[(j+1)*N+i+n-1-j];
 ////                                if(right>best) continue;
 //                            }
-                            temp = ans[j][1] + (left>right?left:right);
-//							temp = ans[j*N+1] + (left>right?left:right);
+//                            temp = ans[j][1] + (left>right?left:right);
+							temp += (left>right?left:right);
                             if(temp < best)
                             	best = temp; 
                     }
-                    ans[i][n] = best;
-//					ans[i*N+n] = best; 
+//                    ans[i][n] = best;
+					ans[i*N+n] = best; 
                     //cout << "ans[" << i << "][" << n << "]=" << best<< endl; 
                 }
             }
-			cout << ans[0][N] << endl; 
-//			cout << ans[N] << endl; 
+//			cout << ans[0][N] << endl; 
+			cout << ans[N] << endl; 
 			time = clock()- time;
 			cout << ((float)time)/CLOCKS_PER_SEC << endl;
         }
